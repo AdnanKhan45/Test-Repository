877e1f3734c655830d78b30a9ce2214fa900e979
Adnan Khan <ak187429@gmail.com>
2021-06-29 12:05:53 -0700

    update

--- lib/scrim_app/pages/Scrim_singleItemView.dart
+++ lib/scrim_app/pages/Scrim_singleItemView.dart
@@ -13,181 +13,325 @@ class SingleItemView extends StatefulWidget {
 
 class _SingleItemViewState extends State<SingleItemView> {
   bool isSelected = true;
+  int switchPage = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
       ),
       body: Container(
         margin: EdgeInsets.symmetric(vertical: 10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Center(
-              child: Container(
-                width: 200,
-                height: 120,
-                child: Image.asset(widget.entity.image),
-              ),
-            ),
-            Center(
-              child: Text(
-                "Lorem Ipusm",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "4212",
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "X-points",
-                      style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
-                    )
-                  ],
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Container(
+                  width: 200,
+                  height: 120,
+                  child: Image.asset(widget.entity.image),
                 ),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "145",
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "Match",
-                      style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
-                    )
-                  ],
+              ),
+              Center(
+                child: Text(
+                  "Lorem Ipusm",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.white),
                 ),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "90%",
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                    Text(
-                      "Winrate",
-                      style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
-                    )
-                  ],
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(
-                    child: Text(
-                  "Lorem Ipsum dolor sit amet\nconsectuer adipiscing alt",
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
-                ))
-              ],
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    height: 45.0,
-                    color: Colors.grey,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
+              ),
+              SizedBox(
+                height: 15.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "4212",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "X-points",
+                        style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "145",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Match",
+                        style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Row(
+                        children: [
+                          Text(
+                            "90%",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Colors.white),
+                          ),
+                        ],
+                      ),
+                      Text(
+                        "Winrate",
+                        style: TextStyle(fontWeight: FontWeight.w500, color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                      child: Text(
+                    "Lorem Ipsum dolor sit amet\nconsectuer adipiscing alt",
+                    maxLines: 2,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Colors.white),
+                  ))
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      height: 45.0,
+                      color: Colors.white38,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                              child: InkWell(
+                                  onTap: () {
+                                    setState(() {
+                                      isSelected = true;
+                                      switchPage = 0;
+                                    });
+                                  },
+                                  child: Icon(
+                                    Icons.local_activity,
+                                    color: isSelected == true ? Colors.lightGreenAccent : Colors.black,
+                                  ))),
+                          Container(
                             child: InkWell(
-                                onTap: () {
-                                  setState(() {
-                                    isSelected = true;
-                                  });
-                                },
-                                child: Icon(
-                                  Icons.local_activity,
-                                  color: isSelected == true ? Colors.lightGreenAccent : Colors.black,
-                                ))),
-                        Container(
-                          child: InkWell(
-                            onTap: () {
-                              setState(() {
-                                isSelected = false;
-                              });
-                            },
-                            child: Icon(
-                              Icons.account_circle,
-                              color: isSelected == false ? Colors.lightGreenAccent : Colors.black,
+                              onTap: () {
+                                setState(() {
+                                  isSelected = false;
+                                  switchPage = 1;
+                                });
+                              },
+                              child: Icon(
+                                Icons.account_circle,
+                                color: isSelected == false ? Colors.lightGreenAccent : Colors.black,
+                              ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 10.0,),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: 10.0),
+                ],
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              switchPage == 0
+                  ? Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Scrim Information",
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15, color: Colors.white),
+                          ),
+                          activityItem(),
+                          activityItem(),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Container(
+                            margin: EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Rules",
+                                  style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text(
+                                  "There is no restrictions on Account Levels,\nHeroes, Skins and Emblems that are used\nwithout exception.",
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13, color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text(
+                                  "X-team is not responsible for lags,\ndisconnections, gadgets not functioning\nproperly and mistakes from the other party.",
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13, color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 15.0,
+                                ),
+                                Text(
+                                  "Additional Rules",
+                                  style: TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text(
+                                  "There is no restrictions on Account Levels,\nHeroes, Skins and Emblems that are used\nwithout exception.",
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13, color: Colors.white),
+                                ),
+                                SizedBox(
+                                  height: 10.0,
+                                ),
+                                Text(
+                                  "X-team is not responsible for lags,\ndisconnections, gadgets not functioning\nproperly and mistakes from the other party.",
+                                  maxLines: 3,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13, color: Colors.white),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  : Text("")
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  activityItem() {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+            decoration: BoxDecoration(color: Colors.black, border: Border.all(width: 1.5, color: Colors.white38)),
+            child: Container(
+              margin: EdgeInsets.all(8.0),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text("Scrim Information", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),),
+                  Icon(Icons.room_rounded, color: Colors.white),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    "Scrim Fee",
+                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500, color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    "2000",
+                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500, color: Colors.white),
+                  )
                 ],
               ),
             ),
-            Expanded(
-              child: GridView.builder(itemCount: 4,gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 1.8), itemBuilder: (context, index) {
-                return Container(
-                  margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(width: 1.5, color: Colors.black)
-                  ),
-                  child: Container(
-                    margin: EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Icon(Icons.room_rounded),
-                        SizedBox(height: 5.0,),
-                        Text("Scrim Fee", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500),),
-                        SizedBox(height: 5.0,),
-                        Text("2000", style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500),)
-                      ],
-                    ),
-                  ),
-                );
-              }),
-            ),
-            SizedBox(height: 15.0,),
-          ],
+          ),
         ),
-      ),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 30.0),
+            margin: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+            decoration: BoxDecoration(color: Colors.black, border: Border.all(width: 1.5, color: Colors.white38)),
+            child: Container(
+              margin: EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(Icons.room_rounded, color: Colors.white),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    "Scrim Fee",
+                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500, color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Text(
+                    "2000",
+                    style: TextStyle(fontSize: 13, fontWeight: FontWeight.w500, color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+
+  }
+
+  listVIewITem() {
+    return Row(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 10.0),
+          width: 40,
+          height: 40,
+          decoration: BoxDecoration(color: Colors.green, shape: BoxShape.circle),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "dummy title",
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: Colors.white),
+            ),
+            Text(
+              "dummy subtitle",
+              style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.white38),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
